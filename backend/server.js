274b35037758ac7e7ccc11e7201f5af715a2766d
M.Hanzala Khaliq <hanzallahkhaliq@gmail.com>
2025-03-03 19:53:57 +0500

    Integrate Signup Api

--- backend/server.js
+++ backend/server.js
@@ -26,6 +26,7 @@ app.use('/api/v1/auth',authRoutes);
 app.use('/api/v1/income',incomeRoutes);
 app.use('/api/v1/expense',expenseRoutes);
 app.use('/api/v1/dashboard',dashboardRoutes);
+app.use("/api/v1/image/", authRoutes);
 
 // serve uploads folder
 app.use('/uploads',express.static(path.join(__dirname,'uploads')));
--- frontend/src/pages/Auth/SignUp.jsx
+++ frontend/src/pages/Auth/SignUp.jsx
@@ -1,82 +1,120 @@
-import React, { useState } from 'react'
-import AuthLayout from '../../components/layouts/AuthLayout'
-import { Link,useNavigate } from 'react-router-dom'
-import Input from '../../components/Inputs/Input'
-import { validateEmail } from '../../utils/helper'
-import ProfilePhotoSelector from '../../components/Inputs/ProfilePhotoSelector'
+import React, { useContext, useState } from "react";
+import AuthLayout from "../../components/layouts/AuthLayout";
+import { Link, useNavigate } from "react-router-dom";
+import Input from "../../components/Inputs/Input";
+import { validateEmail } from "../../utils/helper";
+import ProfilePhotoSelector from "../../components/Inputs/ProfilePhotoSelector";
+import axiosInstance from "../../utils/axiosInstance";
+import { API_PATHS } from "../../utils/apiPath";
+import { UserContext } from "../../context/userContext";
+import uploadImage from "../../utils/uploadImage";
 const SignUp = () => {
-  const [profile,setProfile] = useState(null)
-  const [fullName,setFullName] = useState("")
-  const [email,setEmail] = useState("");
-  const [password,setPassword] = useState("");
+  const [profile, setProfile] = useState(null);
+  const [fullName, setFullName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-  const [error,setError] = useState(null);
+  const [error, setError] = useState(null);
+  const { updateUser } = useContext(UserContext);
 
   const navigate = useNavigate();
   // Handle signup form submit
-  const handleSignUp = async(e) => {
+  const handleSignUp = async (e) => {
     e.preventDefault();
 
     let profileImageUrl = "";
-    if(!fullName){
+    if (!fullName) {
       setError("Please enter your full name");
-      return
+      return;
     }
-    if(!validateEmail(email)){
+    if (!validateEmail(email)) {
       setError("Please enter a valid email address");
-      return
+      return;
     }
 
-    if(!password){
+    if (!password) {
       setError("Please enter a password");
-      return
+      return;
     }
-    setError("")
-  }
+    setError("");
+
+    try {
+      //upload image if present
+      if (profile) {
+        const imgUploadRes = await uploadImage(profile);
+        profileImageUrl = imgUploadRes.imageUrl || "";
+      }
+      const response = await axiosInstance.post(API_PATHS.AUTH.REGISTER, {
+        fullName,
+        email,
+        password,
+        profileImageUrl,
+      });
+      const { token, user } = response.data;
+      if (token) {
+        localStorage.setItem("token", token);
+        updateUser(user);
+        navigate("/dashboard");
+      }
+    } catch (error) {
+      if (error.response && error.response.data.message) {
+        setError(error.response.data.message);
+      } else {
+        setError("Something went wrong. Please try again");
+      }
+    }
+  };
   return (
     <AuthLayout>
-      <div className='lg:w-[100%] h-auto md:h-full mt-10 md:mt-0 flex flex-col justify-center '>
-        <h3 className='text-xl font-semibold text-black'>Create an Account</h3>
-        <p className='text-xs text-slate-700 mt-[5px] mb-6'>Join us today by entering your details below</p>
+      <div className="lg:w-[100%] h-auto md:h-full mt-10 md:mt-0 flex flex-col justify-center ">
+        <h3 className="text-xl font-semibold text-black">Create an Account</h3>
+        <p className="text-xs text-slate-700 mt-[5px] mb-6">
+          Join us today by entering your details below
+        </p>
 
         <form onSubmit={handleSignUp}>
+          <ProfilePhotoSelector image={profile} setImage={setProfile} />
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+            <Input
+              value={fullName}
+              onChange={({ target }) => setFullName(target.value)}
+              label="Full Name"
+              placeholder="John Doe"
+              type="text"
+            />
 
-        <ProfilePhotoSelector image={profile} setImage={setProfile}/>
-          <div className='grid grid-cols-1 md:grid-cols-2 gap-4'>
-           <Input
-            value={fullName}
-            onChange={({target}) => setFullName(target.value)}
-            label="Full Name"
-            placeholder="John Doe"
-            type="text"
-
-           />
-
-<Input
-          value={email}
-          onChange={(e)=>setEmail(e.target.value)}
-          label="Email Address"
-           placeholder="john@example.com"
-            type="text"
-           />
-             <div className="col-span-2">
-             <Input
-          value={password}
-          onChange={(e)=>setPassword(e.target.value)}
-          label="Password"
-           placeholder="Min 8 characters"
-            type="password"
-           />
-             </div>
+            <Input
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              label="Email Address"
+              placeholder="john@example.com"
+              type="text"
+            />
+            <div className="col-span-2">
+              <Input
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                label="Password"
+                placeholder="Min 8 characters"
+                type="password"
+              />
+            </div>
           </div>
 
-               {error && <p className='text-red-500 text-xs pb-2.5'>{error}</p>}
-                     <button type='submit' className='btn-primary cursor-pointer'>SIGN UP</button>
-                     <p className='text-[13px] text-slate-800 mt-3'>Already have an account? <Link className='font-medium text-primary underline' to="/login" >Login</Link></p>
+          {error && <p className="text-red-500 text-xs pb-2.5">{error}</p>}
+          <button type="submit" className="btn-primary cursor-pointer">
+            SIGN UP
+          </button>
+          <p className="text-[13px] text-slate-800 mt-3">
+            Already have an account?{" "}
+            <Link className="font-medium text-primary underline" to="/login">
+              Login
+            </Link>
+          </p>
         </form>
       </div>
     </AuthLayout>
-  )
-}
+  );
+};
 
-export default SignUp
+export default SignUp;
