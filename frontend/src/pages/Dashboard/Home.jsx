996fd994b3f18b5564f549e19f7407ab98b599eb
M.Hanzala Khaliq <hanzallahkhaliq@gmail.com>
2025-03-05 00:54:15 +0500

    Create Dashboard Layout

--- frontend/src/pages/Dashboard/Home.jsx
+++ frontend/src/pages/Dashboard/Home.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import DasboardLayout from "../../components/layouts/DasboardLayout";
+import { userUserAuth } from "../../hooks/useUserAuth";
 
 const Home = () => {
+  userUserAuth();
   return (
-    <div>Home</div>
-  )
-}
+    <DasboardLayout activeMenu="Dashboard">
+      <div className="my-5 mx-auto">Home</div>
+    </DasboardLayout>
+  );
+};
 
 export default Home
